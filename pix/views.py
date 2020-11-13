fe874d3a2fd8d7f0dc3689f1e24dcaf81324e67b
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-13 20:38:50 +0300

    added a user-interface template

--- pix/views.py
+++ pix/views.py
@@ -30,6 +30,32 @@ def location(request,location):
        "title" : title , 
        "breadcrumb" : breadcrumb, 
     }
-    return render(request,'location.html', context )
+    return render(request,'user_interface.html', context )
 
+def category(request,category):
+    images = Image.get_by_category(category)
+    title = category
+    breadcrumb = "Category"
 
+    context = {
+        "images":images ,
+        "title":title , 
+        "breadcrumb": breadcrumb,
+    }
+    return render(request,'user_interface.html', context )
+
+def search_images(request):
+    if 'images' in request.GET and request.GET['images']:
+        search_term = request.GET.get("images")
+        images = Image.search_image(search_term)
+        # status = f"Displaying images related to {search_term} "
+        status = search_term
+        context = {
+            "status":status , 
+            "images":images ,
+            "title":search_term 
+        }
+        return render(request , 'user_interface.html', context )
+    else:
+        status = "Enter an input !"
+        return render(request, "user_interface.html",{"status":status})
