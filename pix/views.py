ccbba697aae8a9cedc75eeff4e41fd71e8f239e2
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-13 20:20:10 +0300

    added a templates folder

--- pix/views.py
+++ pix/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to Wix-gallery')
+# def welcome(request):
+#     return HttpResponse('Welcome to Wix-gallery')
+
+def home(request):
+    title = "Welcome to Wix-gallery"
+    images = Image.fetch_all()
+    locations = Location.fetch_locations()
+    categories = Category.fetch_categories() 
+
+    context = {
+        'title' : title ,
+        'images' : images,
+        'locations' : locations,
+        'categories' : categories,
+    }
+    return render(request,'index.html',context)
+
